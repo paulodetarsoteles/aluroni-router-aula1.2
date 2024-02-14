35ece932dade2685a7c9d155583ffd566b02d6dd
Paulo de Tarso <potassio@hotmail.com>
2024-02-14 18:33:24 -0300

    Ajuste da rota com uso do Outlet

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Sobre from 'pages/Sobre';
 
-export default function AppRouter(){
-  return(
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-        <Route path='/sobre' element={<Sobre />} />
-      </Routes>
-    </Router>
+export default function AppRouter() {
+  return (
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+          <Route path='/sobre' element={<Sobre />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
